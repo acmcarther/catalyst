f40a3409a6a8214723511f55c9b900d89a3ac190
Alex McArther <acmcarther@gmail.com>
2015-08-28 23:22:12 -0700

    remove silly module loading

--- src/listening.rs
+++ src/listening.rs
@@ -1,179 +1,171 @@
-pub use self::listening::{
-  spawn_listener
+use iron::status;
+use iron::prelude::*;
+use iron::error::HttpError;
+use iron::headers::{parsing, Header, HeaderFormat};
+use iron::{BeforeMiddleware, AfterMiddleware, typemap};
+
+use router::Router;
+
+use std::fmt;
+use std::io::Read;
+use std::sync::Mutex;
+use std::ascii::AsciiExt;
+use std::sync::mpsc::Sender;
+
+use github_v3::types::comments::{
+  IssueCommentEvent,
+  PullRequestReviewCommentEvent,
 };
+use github_v3::types::PushEvent;
+use github_v3::types::pull_requests::PullRequestEvent;
 
-mod listening {
-  use iron::prelude::*;
-  use iron::status;
-  use router::Router;
-  use iron::headers::{parsing, Header, HeaderFormat};
-  use iron::error::HttpError;
+use rustc_serialize::{json};
 
-  use std::io::Read;
+fn handle_root(_: &mut Request) -> IronResult<Response> {
+  Ok(Response::with((status::Ok, "Hello from Catalyst. At some point a configuration page will live here.")))
+}
 
-  use std::fmt;
-  use std::ascii::AsciiExt;
-  use std::sync::Mutex;
+#[derive(Clone, PartialEq, Debug)]
+pub struct GithubEventHeader {
+  pub event_name: String
+}
 
-  use github_v3::types::comments::{
-    IssueCommentEvent,
-    PullRequestReviewCommentEvent,
-  };
-  use github_v3::types::pull_requests::PullRequestEvent;
-  use github_v3::types::PushEvent;
-
-  use rustc_serialize::{json};
-  use std::sync::mpsc::Sender;
-
-  use iron::{BeforeMiddleware, AfterMiddleware, typemap};
-
-
-  fn handle_root(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello from Catalyst. At some point a configuration page will live here.")))
+impl Header for GithubEventHeader {
+  fn header_name() -> &'static str {
+      "X-Github-Event"
   }
-
-  #[derive(Clone, PartialEq, Debug)]
-  pub struct GithubEventHeader {
-    pub event_name: String
+  fn parse_header(raw: &[Vec<u8>]) -> Result<GithubEventHeader, HttpError> {
+    parsing::from_one_raw_str(raw).and_then(|s: String| {
+      Ok(GithubEventHeader{event_name: s.to_ascii_lowercase()})
+    })
   }
+}
 
-  impl Header for GithubEventHeader {
-    fn header_name() -> &'static str {
-        "X-Github-Event"
-    }
-    fn parse_header(raw: &[Vec<u8>]) -> Result<GithubEventHeader, HttpError> {
-      parsing::from_one_raw_str(raw).and_then(|s: String| {
-        Ok(GithubEventHeader{event_name: s.to_ascii_lowercase()})
-      })
-    }
+impl HeaderFormat for GithubEventHeader {
+  fn fmt_header(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    f.write_str(&self.event_name)
   }
+}
 
-  impl HeaderFormat for GithubEventHeader {
-    fn fmt_header(&self, f: &mut fmt::Formatter) -> fmt::Result {
-      f.write_str(&self.event_name)
-    }
-  }
+enum EventType {
+  IssueComment,
+  PullRequestReviewComment,
+  PullRequest,
+  Push,
+  UnknownEvent,
+}
 
-  enum EventType {
-    IssueComment,
-    PullRequestReviewComment,
-    PullRequest,
-    Push,
-    UnknownEvent,
-  }
+struct Deserialize;
 
-  struct Deserialize;
+struct EventInfo;
+impl typemap::Key for EventInfo { type Value = EventType; }
 
-  struct EventInfo;
-  impl typemap::Key for EventInfo { type Value = EventType; }
+struct IsIssueComment;
+impl typemap::Key for IsIssueComment { type Value = IssueCommentEvent; }
 
-  struct IsIssueComment;
-  impl typemap::Key for IsIssueComment { type Value = IssueCommentEvent; }
+struct IsPullRequestReviewComment;
+impl typemap::Key for IsPullRequestReviewComment { type Value = PullRequestReviewCommentEvent; }
 
-  struct IsPullRequestReviewComment;
-  impl typemap::Key for IsPullRequestReviewComment { type Value = PullRequestReviewCommentEvent; }
+struct IsPullRequest;
+impl typemap::Key for IsPullRequest { type Value = PullRequestEvent; }
 
-  struct IsPullRequest;
-  impl typemap::Key for IsPullRequest { type Value = PullRequestEvent; }
-
-  struct IsPush;
-  impl typemap::Key for IsPush { type Value = PushEvent; }
+struct IsPush;
+impl typemap::Key for IsPush { type Value = PushEvent; }
 
 
-  impl BeforeMiddleware for Deserialize {
-    fn before(&self, req: &mut Request) -> IronResult<()> {
-      let mut payload = String::new();
-      req.body.read_to_string(&mut payload).unwrap();
+impl BeforeMiddleware for Deserialize {
+  fn before(&self, req: &mut Request) -> IronResult<()> {
+    let mut payload = String::new();
+    req.body.read_to_string(&mut payload).unwrap();
 
-      let headers = req.headers.clone();
-      let event_header = headers.get::<GithubEventHeader>();
-      event_header.map(|header| {
-        match header.event_name.as_ref() {
-          "issue_comment" => {
-            req.extensions.insert::<EventInfo>(EventType::IssueComment);
-            req.extensions.insert::<IsIssueComment>(json::decode(&payload).unwrap());
-          },
-          "pull_request_review_comment" => {
-            req.extensions.insert::<EventInfo>(EventType::PullRequestReviewComment);
-            req.extensions.insert::<IsPullRequestReviewComment>(json::decode(&payload).unwrap());
-          },
-          "pull_request" => {
-            req.extensions.insert::<EventInfo>(EventType::PullRequest);
-            req.extensions.insert::<IsPullRequest>(json::decode(&payload).unwrap());
-          },
-          "push" => {
-            req.extensions.insert::<EventInfo>(EventType::Push);
-            req.extensions.insert::<IsPush>(json::decode(&payload).unwrap());
-          },
-          _ => {req.extensions.insert::<EventInfo>(EventType::UnknownEvent);}
-        }
-      });
-      Ok(())
-    }
-  }
-
-
-  fn handle_webhooks(req: &mut Request) -> IronResult<Response> {
-    let possible_event_type = req.extensions.get::<EventInfo>();
-
-    match possible_event_type {
-      Some(&EventType::IssueComment) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of issue comment\"}"))),
-      Some(&EventType::PullRequestReviewComment) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of pull request review comment\"}"))),
-      Some(&EventType::PullRequest) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of pull request\"}"))),
-      Some(&EventType::Push) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of push\"}"))),
-      Some(&EventType::UnknownEvent) => Ok(Response::with((status::Accepted, "{\"body\":\"Recv an unhandled event\"}"))),
-      None => Ok(Response::with((status::Accepted, "{\"body\":\"No event header provided\"}")))
-    }
-  }
-
-  struct DeliverActionables {
-    issue_comment_tx: Mutex<Sender<IssueCommentEvent>>,
-    pull_request_review_tx: Mutex<Sender<PullRequestReviewCommentEvent>>,
-    pull_request_tx: Mutex<Sender<PullRequestEvent>>,
-  }
-
-  impl AfterMiddleware for DeliverActionables {
-    fn after(&self, req: &mut Request, response: Response) -> IronResult<Response> {
-      let possible_event_type = req.extensions.remove::<EventInfo>();
-      match possible_event_type {
-        Some(EventType::IssueComment) => {
-          let possible_payload = req.extensions.remove::<IsIssueComment>();
-          possible_payload.map(|payload: IssueCommentEvent| self.issue_comment_tx.lock().map(|sender| sender.send(payload)));
-
+    let headers = req.headers.clone();
+    let event_header = headers.get::<GithubEventHeader>();
+    event_header.map(|header| {
+      match header.event_name.as_ref() {
+        "issue_comment" => {
+          req.extensions.insert::<EventInfo>(EventType::IssueComment);
+          req.extensions.insert::<IsIssueComment>(json::decode(&payload).unwrap());
         },
-        Some(EventType::PullRequestReviewComment) => {
-          let possible_payload = req.extensions.remove::<IsPullRequestReviewComment>();
-          possible_payload.map(|payload: PullRequestReviewCommentEvent| self.pull_request_review_tx.lock().map(|sender| sender.send(payload)));
+        "pull_request_review_comment" => {
+          req.extensions.insert::<EventInfo>(EventType::PullRequestReviewComment);
+          req.extensions.insert::<IsPullRequestReviewComment>(json::decode(&payload).unwrap());
         },
-        Some(EventType::PullRequest) => {
-          let possible_payload = req.extensions.remove::<IsPullRequest>();
-          possible_payload.map(|payload: PullRequestEvent| self.pull_request_tx.lock().map(|sender| sender.send(payload)));
+        "pull_request" => {
+          req.extensions.insert::<EventInfo>(EventType::PullRequest);
+          req.extensions.insert::<IsPullRequest>(json::decode(&payload).unwrap());
         },
-        _ => ()
+        "push" => {
+          req.extensions.insert::<EventInfo>(EventType::Push);
+          req.extensions.insert::<IsPush>(json::decode(&payload).unwrap());
+        },
+        _ => {req.extensions.insert::<EventInfo>(EventType::UnknownEvent);}
       }
-      Ok(response)
+    });
+    Ok(())
+  }
+}
+
+
+fn handle_webhooks(req: &mut Request) -> IronResult<Response> {
+  let possible_event_type = req.extensions.get::<EventInfo>();
+
+  match possible_event_type {
+    Some(&EventType::IssueComment) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of issue comment\"}"))),
+    Some(&EventType::PullRequestReviewComment) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of pull request review comment\"}"))),
+    Some(&EventType::PullRequest) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of pull request\"}"))),
+    Some(&EventType::Push) => Ok(Response::with((status::Accepted, "{\"body\":\"Successful recv of push\"}"))),
+    Some(&EventType::UnknownEvent) => Ok(Response::with((status::Accepted, "{\"body\":\"Recv an unhandled event\"}"))),
+    None => Ok(Response::with((status::Accepted, "{\"body\":\"No event header provided\"}")))
+  }
+}
+
+struct DeliverActionables {
+  issue_comment_tx: Mutex<Sender<IssueCommentEvent>>,
+  pull_request_review_tx: Mutex<Sender<PullRequestReviewCommentEvent>>,
+  pull_request_tx: Mutex<Sender<PullRequestEvent>>,
+}
+
+impl AfterMiddleware for DeliverActionables {
+  fn after(&self, req: &mut Request, response: Response) -> IronResult<Response> {
+    let possible_event_type = req.extensions.remove::<EventInfo>();
+    match possible_event_type {
+      Some(EventType::IssueComment) => {
+        let possible_payload = req.extensions.remove::<IsIssueComment>();
+        possible_payload.map(|payload: IssueCommentEvent| self.issue_comment_tx.lock().map(|sender| sender.send(payload)));
+
+      },
+      Some(EventType::PullRequestReviewComment) => {
+        let possible_payload = req.extensions.remove::<IsPullRequestReviewComment>();
+        possible_payload.map(|payload: PullRequestReviewCommentEvent| self.pull_request_review_tx.lock().map(|sender| sender.send(payload)));
+      },
+      Some(EventType::PullRequest) => {
+        let possible_payload = req.extensions.remove::<IsPullRequest>();
+        possible_payload.map(|payload: PullRequestEvent| self.pull_request_tx.lock().map(|sender| sender.send(payload)));
+      },
+      _ => ()
     }
+    Ok(response)
   }
+}
 
-  pub fn spawn_listener(
-    issue_comment_tx: Sender<IssueCommentEvent>,
-    pull_request_tx: Sender<PullRequestEvent>,
-    pull_request_review_tx: Sender<PullRequestReviewCommentEvent>
-    ) -> Iron<Router> {
+pub fn spawn_listener(
+  issue_comment_tx: Sender<IssueCommentEvent>,
+  pull_request_tx: Sender<PullRequestEvent>,
+  pull_request_review_tx: Sender<PullRequestReviewCommentEvent>
+  ) -> Iron<Router> {
 
-    let deliverer = DeliverActionables {
-      issue_comment_tx: Mutex::new(issue_comment_tx),
-      pull_request_tx: Mutex::new(pull_request_tx),
-      pull_request_review_tx: Mutex::new(pull_request_review_tx),
-    };
-    let mut webhook_chain = Chain::new(handle_webhooks);
-    webhook_chain.link_before(Deserialize);
-    webhook_chain.link_after(deliverer);
+  let deliverer = DeliverActionables {
+    issue_comment_tx: Mutex::new(issue_comment_tx),
+    pull_request_tx: Mutex::new(pull_request_tx),
+    pull_request_review_tx: Mutex::new(pull_request_review_tx),
+  };
+  let mut webhook_chain = Chain::new(handle_webhooks);
+  webhook_chain.link_before(Deserialize);
+  webhook_chain.link_after(deliverer);
 
-    let mut router = Router::new();
-    router.get("/", handle_root);
-    router.post("/github_webhooks", webhook_chain);
+  let mut router = Router::new();
+  router.get("/", handle_root);
+  router.post("/github_webhooks", webhook_chain);
 
-    Iron::new(router)
-  }
+  Iron::new(router)
 }
--- src/sending.rs
+++ src/sending.rs
@@ -1,84 +1,76 @@
-pub use self::sending::{
-  spawn_sender
+use github_v3::IssueCommenter;
+use github_v3::Authorization;
+use github_v3::github_client::GithubClient;
+use github_v3::types::repos::Repository;
+use github_v3::types::pull_requests::{
+  PullRequestEvent,
+};
+use github_v3::types::comments::{
+  IssueCommentEvent,
+  CreateIssueComment,
+  PullRequestReviewCommentEvent,
 };
 
-mod sending {
+use std::env;
+use std::thread;
+use std::thread::JoinHandle;
+use std::collections::HashMap;
+use std::sync::mpsc::{Receiver, TryRecvError};
 
-  use github_v3::IssueCommenter;
-  use github_v3::Authorization;
-  use github_v3::github_client::GithubClient;
-  use github_v3::types::repos::Repository;
-  use github_v3::types::pull_requests::{
-    PullRequestEvent,
-  };
-  use github_v3::types::comments::{
-    IssueCommentEvent,
-    CreateIssueComment,
-    PullRequestReviewCommentEvent,
-  };
+use itertools::Itertools;
 
-  use std::env;
-  use std::thread;
-  use std::thread::JoinHandle;
-  use std::collections::HashMap;
-  use std::sync::mpsc::{Receiver, TryRecvError};
+type RepoFullName = String;
+type RepoStatistics = ();
 
-  use itertools::Itertools;
+fn contains_monitored_repo(event: &IssueCommentEvent, monitored_repos: &HashMap<RepoFullName, RepoStatistics>) -> bool {
+  monitored_repos.contains_key(&event.repository.full_name)
+}
 
-  type RepoFullName = String;
-  type RepoStatistics = ();
+pub fn spawn_sender(
+  issue_comment_rx: Receiver<IssueCommentEvent>,
+  pull_request_rx: Receiver<PullRequestEvent>,
+  pull_request_review_rx: Receiver<PullRequestReviewCommentEvent>
+  ) -> JoinHandle<()> {
 
-  fn contains_monitored_repo(event: &IssueCommentEvent, monitored_repos: &HashMap<RepoFullName, RepoStatistics>) -> bool {
-    monitored_repos.contains_key(&event.repository.full_name)
-  }
+  let token = env::var("CATALYST_GITHUB_OAUTH_TOKEN").unwrap();
+  let repo_owner = env::var("CATALYST_REPO_OWNER").unwrap();
+  let repo_name = env::var("CATALYST_REPO_NAME").unwrap();
 
-  pub fn spawn_sender(
-    issue_comment_rx: Receiver<IssueCommentEvent>,
-    pull_request_rx: Receiver<PullRequestEvent>,
-    pull_request_review_rx: Receiver<PullRequestReviewCommentEvent>
-    ) -> JoinHandle<()> {
+  let mut monitored_repos = HashMap::new();
+  monitored_repos.insert(repo_owner.clone() + "/" + &repo_name, ());
 
-    let token = env::var("CATALYST_GITHUB_OAUTH_TOKEN").unwrap();
-    let repo_owner = env::var("CATALYST_REPO_OWNER").unwrap();
-    let repo_name = env::var("CATALYST_REPO_NAME").unwrap();
+  let client = GithubClient::new(Some(Authorization("token ".to_owned() + &token)));
 
-    let mut monitored_repos = HashMap::new();
-    monitored_repos.insert(repo_owner.clone() + "/" + &repo_name, ());
+  thread::spawn (move || {
+    let mut channels_up = true;
+    while channels_up {
+      let possible_issue_comment = issue_comment_rx.try_recv();
+      let possible_pull_request = pull_request_rx.try_recv();
+      let possible_pull_request_review = pull_request_review_rx.try_recv();
 
-    let client = GithubClient::new(Some(Authorization("token ".to_owned() + &token)));
+      let _ = possible_issue_comment
+        .map_err(|err| if err == TryRecvError::Disconnected {channels_up = false})
+        .ok()
+        .iter()
+        .filter(|issue_comment| contains_monitored_repo(issue_comment, &monitored_repos))
+        .foreach(|issue_comment| {
+          if issue_comment.comment.body.clone().contains("pt r?") {
+            let issue_id = issue_comment.issue.number.clone();
+            let name = issue_comment.repository.name.clone();
+            let owner = issue_comment.repository.owner.login.clone();
+            let repo = Repository{ owner: owner, repo_name: name };
+            let response = CreateIssueComment { body: "PTBOT: Assigning @acmcarther to this PR".to_owned() };
+            println!("LOG: Received a request for reviewers on issue {}, assigning @acmcarther", issue_id);
+            let _ = client.create_comment(repo, issue_id, response);
+          }
+        });
+      let _ = possible_pull_request.map_err(|err| if err == TryRecvError::Disconnected {channels_up = false});
+      let _ = possible_pull_request_review.map_err(|err| if err == TryRecvError::Disconnected {channels_up = false});
 
-    thread::spawn (move || {
-      let mut channels_up = true;
-      while channels_up {
-        let possible_issue_comment = issue_comment_rx.try_recv();
-        let possible_pull_request = pull_request_rx.try_recv();
-        let possible_pull_request_review = pull_request_review_rx.try_recv();
+      //possible_pull_request.map(|err| if err == TryRecvError::Disconnected {channels_up = false});
+      //possible_pull_request_review.map(|err| if err == TryRecvError::Disconnected {channels_up = false});
 
-        let _ = possible_issue_comment
-          .map_err(|err| if err == TryRecvError::Disconnected {channels_up = false})
-          .ok()
-          .iter()
-          .filter(|issue_comment| contains_monitored_repo(issue_comment, &monitored_repos))
-          .foreach(|issue_comment| {
-            if issue_comment.comment.body.clone().contains("pt r?") {
-              let issue_id = issue_comment.issue.number.clone();
-              let name = issue_comment.repository.name.clone();
-              let owner = issue_comment.repository.owner.login.clone();
-              let repo = Repository{ owner: owner, repo_name: name };
-              let response = CreateIssueComment { body: "PTBOT: Assigning @acmcarther to this PR".to_owned() };
-              println!("LOG: Received a request for reviewers on issue {}, assigning @acmcarther", issue_id);
-              let _ = client.create_comment(repo, issue_id, response);
-            }
-          });
-        let _ = possible_pull_request.map_err(|err| if err == TryRecvError::Disconnected {channels_up = false});
-        let _ = possible_pull_request_review.map_err(|err| if err == TryRecvError::Disconnected {channels_up = false});
-
-        //possible_pull_request.map(|err| if err == TryRecvError::Disconnected {channels_up = false});
-        //possible_pull_request_review.map(|err| if err == TryRecvError::Disconnected {channels_up = false});
-
-        thread::sleep_ms(2000);
-      }
-    })
-  }
-
+      thread::sleep_ms(2000);
+    }
+  })
 }
